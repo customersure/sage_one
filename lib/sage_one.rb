ce51824da8bb9c4d63309ba3fd870e0fc776d770
Luke Brown <tsdbrown@gmail.com>
2012-11-08 16:52:42 +0000

    Added specs for SageOne methods

--- lib/sage_one.rb
+++ lib/sage_one.rb
@@ -14,14 +14,15 @@ module SageOne
       SageOne::Client.new(options)
     end
 
+    # True if the method can be delegated to the SageOne::Client
+    def respond_to?(method, include_private=false)
+      new.respond_to?(method, include_private) || super(method, include_private)
+    end
+
     # Delegate to SageOne::Client.new
     def method_missing(method, *args, &block)
       return super unless new.respond_to?(method)
       new.send(method, *args, &block)
-    end
-
-    def respond_to?(method, include_private=false)
-      new.respond_to?(method, include_private) || super(method, include_private)
     end
   end
 end
--- spec/sage_one_spec.rb
+++ spec/sage_one_spec.rb
@@ -1,7 +1,42 @@
 require 'spec_helper'
 
 describe SageOne do
+
   it 'has a version' do
     SageOne::VERSION.should_not be_nil
   end
+
+  describe '.new' do
+    it { SageOne.new.should be_a(SageOne::Client) }
+  end
+
+  describe '.respond_to?' do
+    it 'returns true when the method exists' do
+      expect(SageOne.respond_to?(:new, true)).to be_true
+    end
+    it 'returns true when a client responds to the method' do
+      expect(SageOne.respond_to?(:sales_invoices)).to be_true
+    end
+    it 'returns false when the method does not exist' do
+      expect(SageOne.respond_to?(:noway, true)).to be_false
+    end
+  end
+
+  describe '.method_missing' do
+    context "when the client does not respond to the method either" do
+      it 'raises a NoMethodError' do
+        expect(lambda{ SageOne.noway }).to raise_error(NoMethodError)
+      end
+    end
+    context "when the client does respond to the method" do
+      it 'delegates the call to the client, passing any args and block' do
+        block = Proc.new { puts "hi" }
+        mock_client = mock('SageOne::Client', :my_method => 'called')
+        SageOne.should_receive(:new).twice.and_return(mock_client)
+        mock_client.should_receive(:my_method).with('abc', block)
+        SageOne.my_method('abc', block)
+      end
+    end
+  end
+
 end
