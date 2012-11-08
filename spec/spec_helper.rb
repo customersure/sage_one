5cce71f9b8a1cb1f62b0708e99769e2150d65e08
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2012-11-08 18:14:22 +0000

    Working oauth helpers and spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,42 @@
-require 'rspec'
+unless ENV['CI']
+  require 'simplecov'
+  SimpleCov.start do
+    add_filter "/spec"
+  end
+end
+
 require 'sage_one'
+require 'rspec'
+require 'webmock/rspec'
 
 RSpec.configure do |config|
   config.color_enabled = true
+end
+
+def stub_delete(url)
+  stub_request(:delete, sage_url(url))
+end
+
+def stub_get(url)
+  stub_request(:get, sage_url(url))
+end
+
+def stub_post(url)
+  stub_request(:post, sage_url(url))
+end
+
+def stub_put(url)
+  stub_request(:put, sage_url(url))
+end
+
+def sage_url(url)
+  "https://app.sageone.com/api/v1#{url}"
+end
+
+def fixture_path
+  File.expand_path("../fixtures", __FILE__)
+end
+
+def fixture(file)
+  File.new(fixture_path + '/' + file)
 end
