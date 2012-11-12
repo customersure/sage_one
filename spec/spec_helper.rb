0920f805101bc371a807a9760eb6ce140c325837
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2012-11-12 16:01:17 +0000

    Enable :focus in spec_helper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,6 +12,8 @@ require 'webmock/rspec'
 RSpec.configure do |config|
   config.color_enabled = true
   config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.filter_run :focus => true
+  config.run_all_when_everything_filtered = true
 end
 
 def a_delete(url)
