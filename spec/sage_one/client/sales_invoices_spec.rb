28bf0bf8186f87dc3d404ff75b74c6bb6393083a
Chris Stainthorpe <chris.stainthorpe@customersure.com>
2012-11-13 14:55:36 +0000

    Skeleton spec for sales invoices

---
+++ spec/sage_one/client/sales_invoices_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe SageOne::SalesInvoices do
+
+  describe 'sales_invoices' do
+
+  end
+
+  describe 'create_sales_invoice' do
+
+  end
+
+  describe 'sales_invoice' do
+
+  end
+
+  describe 'update_sales_invoice' do
+
+  end
+
+  describe 'delete_sales_invoice!' do
+
+  end
+
+end
